aee9e62965e451e6f38bc48606f0153e53672761
arderth <33568080+arderth@users.noreply.github.com>
2017-11-22 12:21:05 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ sudo rm /boot/cmdline.txt
 sudo touch /boot/cmdline.txt
 sudo sh -c "echo 'consoleblank=1 logo.nologo dwc_otg.lpm_enable=0 console=tty1 root=PARTUUID=84fa8189-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait  quiet   plymouth.ignore-serial-consoles loglevel=1 quiet' >> /boot/cmdline.txt"
 sudo python -m pip install pyserial
-cp /home/pi/piSignagePro/rs232/TVon.py /home/pi/TVon.py
-cp /home/pi/piSignagePro/rs232/TVoff.py /home/pi/TVoff.py
+sudo cp /home/pi/piSignagePro/rs232/TVon.py /home/pi/TVon.py
+sudo cp /home/pi/piSignagePro/rs232/TVoff.py /home/pi/TVoff.py
 touch mycron
 echo "# m h  dom mon dow   command" >> mycron
 echo "@reboot python /home/pi/TVon.py" >> mycron
@@ -13,7 +13,4 @@ crontab mycron
 rm mycron
 sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
 sudo cp /home/pi/piSignagePro/rs232/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
-sed -i -e 's/.*"config_server".*/    "config_server": "englassmedya.com",/g'   -e 's/.*"media_server".*/    "media_server":   "englassmedya.com",/g'  /home/pi/piSignagePro/package.json
-sudo rm /etc/resolv.conf
-sudo cp /home/pi/piSignagePro/rs232/resolv.conf /etc/resolv.conf
 echo "********Islem Tamamlandi.**********"
